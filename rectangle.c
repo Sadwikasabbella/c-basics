39dd08e72af3f61aebc97b388bc8f6e21e0e6764
Sadwikasabbella <96563572+Sadwikasabbella@users.noreply.github.com>
2021-12-23 12:06:00 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int l,b,area,perimeter;
+	scanf("%d %d",&l,&b);
+	area=l*b;
+	perimeter=2*(l+b);
+	printf("area is:%d\n",area);
+    printf("perimeter is:%d",perimeter);
+}
